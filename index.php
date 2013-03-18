cfa91edb40e3dc5952bf8e7a1ff7a3ad1cb8bca4
Jon Amar <jon@solomongiles.com>
2013-03-18 14:23:07 +0100

    + color & border variables; tweaked heading typography; + images for credits; improved credits markup; + BL .svg logo; + working styles for credits

--- index.php
+++ index.php
@@ -1,29 +1,74 @@
 <?php include 'inc/_atrio.caput.php' ?>
 
 
-<div class="wrapper"> 
+<div class="wrapper">
 
-	<hgroup class="r-all"> 
+	<div class="g1">
+		<img class="logo" src="img/Blink-the-Series.svg" />
+	</div>
+
+	<hgroup class="g2-g3">
 		<h1>Credits</h1>	
 		<h2>Filled with intrigue!!</h2>
 	</hgroup>
 	
-		<div class="g1"> 
-			<img src="img/malachi.png"/>
-			<h2>Malachi Rempen</h2>
-			<p>Malachi Rempen makes <a href="http://www.mmrempen.com">movies</a>, a weekly <a href="http://www.itchyfeetcomic.com">web comic</a>, and enjoys changes in temperature and color. Observant passers-by will notice him wandering around Berlin, deep in thought.</p>
+
+		<div class="g1 comic_panel"> 
+			<img src="img/people/rachael.jpg"/>
+			<div class="comic_caption border-top">
+				<h2>Rachael Newport</h2>
+				<h4>DP, Camera, Writer E3 & 4, Story</h4>
+				<p>Malachi Rempen makes <a href="http://www.mmrempen.com">movies</a>, a weekly <a href="http://www.itchyfeetcomic.com">web comic</a>, and enjoys changes in temperature and color. Observant passers-by will notice him wandering around Berlin, deep in thought.</p>
+			</div>
 		</div>
-<!--
-		<div class="g2">
-			<img src="img/malachi.png"/>
-			<p>Malachi Rempen makes <a href="http://www.mmrempen.com">movies</a>, a weekly <a href="http://www.itchyfeetcomic.com">web comic</a>, and enjoys changes in temperature and color. Observant passers-by will notice him wandering around Berlin, deep in thought.</p>
+
+		<div class="g2 comic_panel"> 
+			<img src="img/people/malachi.jpg"/>
+			<div class="comic_caption border-top">
+				<h2>Malachi Rempen</h2>
+				<h4>Director: E3, 56, Sound Assistant</h4>
+				<p>Malachi Rempen makes <a href="http://www.mmrempen.com">movies</a>, a weekly <a href="http://www.itchyfeetcomic.com">web comic</a>, and enjoys changes in temperature and color. Observant passers-by will notice him wandering around Berlin, deep in thought.</p>
+			</div>
 		</div>
-		
-		<div class="g3">
-			<img src="img/malachi.png"/>
-			<p>Malachi Rempen makes <a href="http://www.mmrempen.com">movies</a>, a weekly <a href="http://www.itchyfeetcomic.com">web comic</a>, and enjoys changes in temperature and color. Observant passers-by will notice him wandering around Berlin, deep in thought.</p>
+
+
+		<div class="g3 comic_panel"> 
+			<img src="img/people/christine.jpg"/>
+			<div class="comic_caption border-top">
+				<h2>Christine Utterberg</h2>
+				<h4>DP, Camera, Writer E3 & 4, Story</h4>
+				<p>Malachi Rempen makes <a href="http://www.mmrempen.com">movies</a>, a weekly <a href="http://www.itchyfeetcomic.com">web comic</a>, and enjoys changes in temperature and color. Observant passers-by will notice him wandering around Berlin, deep in thought.</p>
+			</div>
 		</div>
--->
+
+
+		<div class="g1 comic_panel"> 
+			<img src="img/people/david-m.jpg"/>
+			<div class="comic_caption border-top">
+				<h2>David Masterson</h2>
+				<h4>Actor: Crow</h4>
+				<p>Malachi Rempen makes <a href="http://www.mmrempen.com">movies</a>, a weekly <a href="http://www.itchyfeetcomic.com">web comic</a>, and enjoys changes in temperature and color. Observant passers-by will notice him wandering around Berlin, deep in thought.</p>
+			</div>
+		</div>
+
+		<div class="g2 comic_panel"> 
+			<img src="img/people/andrew.jpg"/>
+			<div class="comic_caption border-top">
+				<h2>Andrew James</h2>
+				<h4>DP, Camera, Writer E3 & 4, Story</h4>
+				<p>Malachi Rempen makes <a href="http://www.mmrempen.com">movies</a>, a weekly <a href="http://www.itchyfeetcomic.com">web comic</a>, and enjoys changes in temperature and color. Observant passers-by will notice him wandering around Berlin, deep in thought.</p>
+			</div>
+		</div>
+
+
+		<div class="g3 comic_panel"> 
+			<img src="img/people/rachael.jpg"/>
+			<div class="comic_caption border-top">
+				<h2>Christine Utterberg</h2>
+				<h4>DP, Camera, Writer E3 & 4, Story</h4>
+				<p>Malachi Rempen makes <a href="http://www.mmrempen.com">movies</a>, a weekly <a href="http://www.itchyfeetcomic.com">web comic</a>, and enjoys changes in temperature and color. Observant passers-by will notice him wandering around Berlin, deep in thought.</p>
+			</div>
+		</div>
 
 </div>
 
