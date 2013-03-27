46cb067a255e338b52e2ef32b3455589041f2d59
Jon Amar <jon@solomongiles.com>
2013-03-27 08:01:30 +0100

    integrated fancy box js into compelled.js; removed old/unused lightbox files

--- fancybox.php
+++
@@ -1,7 +0,0 @@
-<?php include 'inc/_atrio.caput.php' ?>
-<?php include 'inc/_corpus.caput.php' ?>
-
-<a class="fancybox-media" href="http://www.youtube.com/watch?v=opj24KnzrWo">Youtube</a>
-
-<?php include 'inc/_corpus.pes.php' ?>
-<?php include 'inc/_atrio.pes.php' ?>
--- inc/_atrio.pes.php
+++ inc/_atrio.pes.php
@@ -1,16 +1,6 @@
     <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
-  
-    <!-- color box -->	
-<!--
-    <script src="http://gsa.blinktheseries.dev/js/jquery.colorbox-min.js"></script>
-		<script>
-			$(document).ready(function(){
-				$(".youtube").colorbox({iframe:true, innerWidth:425, innerHeight:344, returnFocus:false, width: "50%"});
-			});
-		</script>
--->
-		
-<!--
+ 		
+    <!--
     <script src="js/jquery.fitvids.min.js"></script>       
 			<script>
 			  $(document).ready(function(){
@@ -18,38 +8,34 @@
 			    $("#testDiv").fitVids();
 			  });
 			</script>
--->
+			-->
 
-	<!-- Add fancyBox main JS -->
-	<script type="text/javascript" src="/js/jquery.fancybox.pack.js"></script>
-	<!-- Add Media helper (this is optional) -->
-	<script type="text/javascript" src="/js/jquery.fancybox-media.js?v=1.0.5"></script>
-
-	<script type="text/javascript">
-		$(document).ready(function() {
-
-			/*
-			 *  Media helper. Group items, disable animations, hide arrows, enable media and button helpers.
-			*/
-			$('.fancybox-media')
-				.attr('rel', 'media-gallery')
-				.fancybox({
-					openEffect : 'fade',
-					closeEffect : 'none',
-					prevEffect : 'none',
-					nextEffect : 'none', 
-
-					arrows : false,
-					helpers : {
-						media : {},
-					}
-				});
-
-		});
-	</script>
-		
 		
     <script src="js/compiled.js"></script>       
+
+			<!-- Fancy Box -->
+			<script type="text/javascript">
+				$(document).ready(function() {
+		
+					/*
+					 *  Media helper. Group items, disable animations, hide arrows, enable media and button helpers.
+					*/
+					$('.fancybox-media')
+						.attr('rel', 'media-gallery')
+						.fancybox({
+							openEffect : 'fade',
+							closeEffect : 'none',
+							prevEffect : 'none',
+							nextEffect : 'none', 
+		
+							arrows : false,
+							helpers : {
+								media : {},
+							}
+						});
+		
+				});
+			</script>
 
     <script>
         var _gaq=[['_setAccount','UA-XXXXX-X'],['_trackPageview']];
