748156992ffa7df6bd0b0f25cfadcd3fba787536
Jon Amar <jon@solomongiles.com>
2013-03-27 12:12:16 +0100

    Undo change - FTPloy seems to be working

--- inc/_corpus.caput.php
+++ inc/_corpus.caput.php
@@ -6,7 +6,7 @@
 	<nav class="r2 t-all m1">
 		<ul class="panel">
 		<li><a class="fancybox-media" href="http://www.youtube.com/embed/k9prm5BjrUU?rel=0&amp;wmode=transparent">Trailer</a</li>
-		<li><a class="credits" href="/credits">Credit</a></li><br/>
+		<li><a class="credits" href="/credits">Credits</a></li><br/>
 		<li><a href="http://www.youtube.com/user/BlinkTheSeries"><i class="icon-youtube"></i></a></li>
 		<li><a href="https://twitter.com/BlinktheSeries"><i class="icon-twitter-squared"></i></a></li>
 		<li><a href="http://facebook.com/BlinktheSeries"><i class="icon-facebook-squared"></i></a></li>
