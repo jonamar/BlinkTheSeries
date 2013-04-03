3856a99716f9a383674b90dd21607e6fd59a7cbe
Jon Amar <jon@solomongiles.com>
2013-04-03 13:32:48 +0200

    episode 2 design; archive e1 on week1.php

--- week1.php
+++ week1.php
@@ -12,12 +12,9 @@
 	</div>
 
 	<div class="l1 t-all m1 e2 panel">
-			<a class='play fancybox-media' href="http://youtu.be/IyvMOkmuOUE">&#9658; Watch Episode 2</a>
-			<p class="caption caption-1">He found his shirt, but danger lurks.</p>
-<!--
+			<p class="play"><small>Watch Episode Two</small> NEXT WEDNESDAY <small>April 3rd</small></p>
 			<p class="caption caption-1">Who is the mysterious woman in the window and will Jon ever wear pants again?</p>
 			<p class="caption caption-2">Tune in next week for answers to these, and many more, questions on <span class="type-logo"> Blink The Series</span>!!</p>
--->
 	</div>
 
 <?php include 'inc/_corpus.pes.php' ?>
