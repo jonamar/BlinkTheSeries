eba74de7de56bcdce8cc3a2ee0e8a506b972ed0d
Jon Amar <jon@solomongiles.com>
2013-04-10 17:48:50 +0200

    Archived week2

---
+++ week2.php
@@ -0,0 +1,27 @@
+<?php include 'inc/_atrio.caput.php' ?>
+<?php include 'inc/_corpus.caput.php' ?>
+
+	<div class="g-all t-all m1 e1 panel">
+			<p class="caption">Birds are chirping. It's early morning and the start of a beautiful summer day in Berlin.</p>
+			<div class="false-border-bottom"></div>
+			<img class="e1-annie" src="img/e1-annie.png" alt="Psy Vamp" />
+			<div class="false-border-top"></div>
+			<div class="e1-blink"></div>
+			
+			<a class='play fancybox-media' href="http://www.youtube.com/watch?v=5SjtrUYRYx4">&#9658; Watch Episode 1</a>
+	</div>
+
+	<div class="l1 t-all m1 e2 panel">
+			<a class='play fancybox-media' href="http://youtu.be/IyvMOkmuOUE">&#9658; Watch Episode 2</a>
+			<p class="caption caption-1">He found his shirt, but danger lurks.</p>
+	</div>
+
+	<div class="l2 t-all m1 e3 panel">
+			<p class="caption caption-1">Color me curious.</p>
+			<p class="play"><small>Watch Episode Three</small> NEXT WEDNESDAY <small>April 10th</small></p>
+	</div>
+	
+	
+
+<?php include 'inc/_corpus.pes.php' ?>
+<?php include 'inc/_atrio.pes.php' ?>
