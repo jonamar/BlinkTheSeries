c0700f2edb7b4f2d172fcaf9c5a905c8c8739209
Jon Amar <jon@solomongiles.com>
2013-04-17 15:00:33 +0200

    archived week 3

---
+++ week3.php
@@ -0,0 +1,32 @@
+<?php include 'inc/_atrio.caput.php' ?>
+<?php include 'inc/_corpus.caput.php' ?>
+
+	<div class="g-all t-all m1 e1 panel">
+			<p class="caption">Birds are chirping. It's early morning and the start of a beautiful summer day in Berlin.</p>
+			<div class="false-border-bottom"></div>
+			<img class="e1-annie" src="img/e1-annie.png" alt="Psy Vamp" />
+			<div class="false-border-top"></div>
+			<div class="e1-blink"></div>
+			
+			<a class='play fancybox-media' href="http://www.youtube.com/watch?v=5SjtrUYRYx4">&#9658; Watch Episode 1</a>
+	</div>
+
+	<div class="l1 t-all m1 e2 panel">
+			<a class='play fancybox-media' href="http://youtu.be/IyvMOkmuOUE">&#9658; Watch Episode 2</a>
+			<p class="caption caption-1">He found his shirt, but danger lurks.</p>
+	</div>
+
+	<div class="l2 t-all m1 e3 panel">
+			<p class="caption caption-1">Color me curious.</p>
+			<a class='play fancybox-media' href="http://www.youtube.com/watch?v=HoHvSaa9mzQ">&#9658; Watch Episode 3</a>
+			<div class="e3-drawing-top"></div>
+	</div><div class="clear"></div>
+	
+	<div class="r1 t-all m1 e4 panel">
+			<p class="caption caption-1">Is this is the end of the line for our unlikely hero? How much worse can his day get? Tune in next week to find out on <span class="type-logo"> Blink The Series</span>!!</p>
+			<p class="play"><small>Watch Episode Four</small> NEXT WEDNESDAY <small>April 17th</small></p>
+	</div>
+
+
+<?php include 'inc/_corpus.pes.php' ?>
+<?php include 'inc/_atrio.pes.php' ?>
